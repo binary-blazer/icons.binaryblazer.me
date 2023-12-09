3ad3ea7b6dc57e5d945b9bd5f132f6d092add120
Wire List <janjytapyt@sdevs.org>
2023-12-09 22:28:01 +0100

    [✨] format fix

--- public/js/generate.js
+++ public/js/generate.js
@@ -1,25 +1,26 @@
-import feather from 'feather-icons';
-import fs from 'node:fs';
-import path from 'node:path';
-import getIcons from './hooks/getIcons.js';
+import feather from "feather-icons";
+import fs from "node:fs";
+import path from "node:path";
+import getIcons from "./hooks/getIcons.js";
 
 const icons = getIcons();
 
-icons.forEach(icon => {
-    const svg = feather.icons[icon].toSvg();
+icons.forEach((icon) => {
+  const svg = feather.icons[icon].toSvg();
 
-    const file = path.resolve(`./public/icons/${icon}.svg`);
-    fs.writeFileSync(file, svg);
+  const file = path.resolve(`./public/icons/${icon}.svg`);
+  fs.writeFileSync(file, svg);
 });
 
-icons.forEach(icon => {
-    const folder = path.resolve(`./icons/${icon}`);
-    fs.mkdirSync(folder);
+icons.forEach((icon) => {
+  const folder = path.resolve(`./icons/${icon}`);
+  fs.mkdirSync(folder);
 
-    const svg = feather.icons[icon].toSvg();
-    const file = path.resolve(`./icons/${icon}/${icon}.svg`);
-    fs.writeFileSync(file, svg);
+  const svg = feather.icons[icon].toSvg();
+  const file = path.resolve(`./icons/${icon}/${icon}.svg`);
+  fs.writeFileSync(file, svg);
 });
 
-
-console.log(`${icons.length} Icons successfully generated! (public/icons) (icons/)`);
+console.log(
+  `${icons.length} Icons successfully generated! (public/icons) (icons/)`,
+);
--- public/js/remove.js
+++ public/js/remove.js
@@ -1,19 +1,21 @@
-import fs from 'node:fs';
-import path from 'node:path';
+import fs from "node:fs";
+import path from "node:path";
 
-const icons = fs.readdirSync('./icons');
-const iconsPublic = fs.readdirSync('./public/icons');
+const icons = fs.readdirSync("./icons");
+const iconsPublic = fs.readdirSync("./public/icons");
 
-icons.forEach(icon => {
-    if (!iconsPublic.includes(icon)) {
-        fs.rmdirSync(path.resolve(`./icons/${icon}`), { recursive: true });
-    }
+icons.forEach((icon) => {
+  if (!iconsPublic.includes(icon)) {
+    fs.rmdirSync(path.resolve(`./icons/${icon}`), { recursive: true });
+  }
 });
 
-iconsPublic.forEach(icon => {
-    if (!icons.includes(icon)) {
-        fs.unlinkSync(path.resolve(`./public/icons/${icon}`));
-    }
+iconsPublic.forEach((icon) => {
+  if (!icons.includes(icon)) {
+    fs.unlinkSync(path.resolve(`./public/icons/${icon}`));
+  }
 });
 
-console.log(`${icons.length} Icons successfully removed! (public/icons) (icons/)`);
+console.log(
+  `${icons.length} Icons successfully removed! (public/icons) (icons/)`,
+);
